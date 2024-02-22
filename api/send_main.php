6dd297c1bdf20d556dee82f113eabdd96550c47d
DonTraffic <dogerfantom6@gmail.com>
2024-02-22 22:37:55 +0300

    add push in google spreadsheets

--- api/send_main.php
+++
@@ -1,7 +0,0 @@
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=utf-8\r\n";
-
-$message = "Тема: " . $_POST['subject'] . "\n\n";
-$message .= $_POST['message'];
-
-mail($_POST['email'], $_POST['subject'], $message, $headers);
